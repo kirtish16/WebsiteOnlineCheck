c61d91c5df8687ac8d82a86f8420b56d70a1ebe0
Kirtish Surangalikar <55601319+kirtish16@users.noreply.github.com>
2022-05-28 22:22:42 +0530

    Initial commit

---
+++ is-basic/src/main/java/basic/isbasic/IsBasicApplication.java
@@ -0,0 +1,13 @@
+package basic.isbasic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IsBasicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IsBasicApplication.class, args);
+	}
+
+}
---
+++ is-basic/src/test/java/basic/isbasic/IsBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package basic.isbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
